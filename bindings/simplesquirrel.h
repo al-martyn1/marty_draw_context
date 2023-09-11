bccb9c0eb0138f43e9ee8b969e87912626223963
al-martyn1 <amart@mail.ru>
2023-09-11 16:11:44 +0300

    Причесал, запилил DrawCoords

--- bindings/simplesquirrel.h
+++ bindings/simplesquirrel.h
@@ -12,12 +12,11 @@
 // 
 #include "../colorref.h"
 
-
-// ATLASSERT(IsWindow());
-
+//----------------------------------------------------------------------------
 
 
-//---------------------------------------------------------
+
+//----------------------------------------------------------------------------
 /*! \def MARTY_DC_BIND_SQUIRREL_ASSERT(expr)
     Проверка условия и аварийный выход (или сообщение с последующим продолжением работы).
 
@@ -48,37 +47,44 @@
 #endif
 
 
-//---------------------------------------------------------
+//------------------------------
 //! MARTY_DC_BIND_SQUIRREL_ASSERT_FAIL срабатывает всегда, и ставится туда, куда, по идее, никогда попадать не должны
 #define MARTY_DC_BIND_SQUIRREL_ASSERT_FAIL()                        MARTY_DC_BIND_SQUIRREL_ASSERT( 0 )
 
+//----------------------------------------------------------------------------
 
 
 
 
+//----------------------------------------------------------------------------
 
 // marty_draw_context::simplesquirrel::
 namespace marty_draw_context {
 
 namespace simplesquirrel {
 
-namespace utils {
+//----------------------------------------------------------------------------
+
+
 
 //----------------------------------------------------------------------------
+namespace utils {
+
+//------------------------------
 inline
 std::string to_ascii(const char* str)
 {
     return str ? std::string(str) : std::string();
 }
 
-//----------------------------------------------------------------------------
+//------------------------------
 inline
 std::string to_ascii(const std::string &str)
 {
     return str;
 }
 
-//----------------------------------------------------------------------------
+//------------------------------
 inline
 std::string to_ascii(const std::wstring &str)
 {
@@ -88,14 +94,14 @@ std::string to_ascii(const std::wstring &str)
     return strRes;
 }
 
-//----------------------------------------------------------------------------
+//------------------------------
 inline
 std::string to_ascii(const wchar_t* str)
 {
     return str ? to_ascii(std::wstring(str)) : std::string();
 }
 
-//----------------------------------------------------------------------------
+//------------------------------
 struct CToAscii
 {
     std::string operator()(const char* str) const
@@ -124,25 +130,25 @@ struct CToAscii
 
 }; // struct CToAscii
 
-//----------------------------------------------------------------------------
+//------------------------------
 
 
 
-//----------------------------------------------------------------------------
+//------------------------------
 inline
 std::wstring to_wide(const wchar_t* str)
 {
     return str ? std::wstring(str) : std::wstring();
 }
 
-//----------------------------------------------------------------------------
+//------------------------------
 inline
 std::wstring to_wide(const std::wstring &str)
 {
     return str;
 }
 
-//----------------------------------------------------------------------------
+//------------------------------
 inline
 std::wstring to_wide(const std::string &str)
 {
@@ -152,14 +158,14 @@ std::wstring to_wide(const std::string &str)
     return strRes;
 }
 
-//----------------------------------------------------------------------------
+//------------------------------
 inline
 std::wstring to_wide(const char* str)
 {
     return str ? to_wide(std::string(str)) : std::wstring();
 }
 
-//----------------------------------------------------------------------------
+//------------------------------
 struct CToWide
 {
     std::wstring operator()(const wchar_t* str) const
@@ -184,14 +190,17 @@ struct CToWide
 
 }; // struct CToWide
 
+//------------------------------
+
 
 } // namespace utils
 
+//----------------------------------------------------------------------------
 
 
 
 
-
+//----------------------------------------------------------------------------
 struct DrawColor : public ColorRef
 {
 
@@ -199,7 +208,6 @@ struct DrawColor : public ColorRef
     DrawColor(ColorRef clr) : ColorRef(clr) {}
     DrawColor(const DrawColor &clr) : ColorRef(clr) {}
     DrawColor(DrawColor &&clr)      : ColorRef(clr) {}
-
 
 
     static
@@ -242,17 +250,6 @@ struct DrawColor : public ColorRef
         return (int)(std::int32_t)toUnsigned();
     }
 
-    static
-    int rotate() { return 10; }
-
-    static
-    int rotateI(int i) { return i; }
-
-    // std::uint32_t toUnsigned() const
-    // static ColorRef fromUnsigned(std::uint32_t crul)
-    // static ColorRef deserialize(std::string str)
-    // std::string serialize() const
-
     static ssq::Class expose(ssq::VM& vm, const ssq::sqstring &className = _SC("DrawColor"))
     {
         const bool staticMethod = true ;
@@ -266,24 +263,20 @@ struct DrawColor : public ColorRef
                               , true // release
                               );
 
-        cls.addFunc(_SC("toUnsigned")  , &DrawColor::toUnsigned);
-        cls.addFunc(_SC("fromUnsigned"), &DrawColor::fromUnsignedBindHelper);
+        cls.addFunc( _SC("toUnsigned")  , &DrawColor::toUnsigned);
+        cls.addFunc( _SC("fromUnsigned"), &DrawColor::fromUnsignedBindHelper);
 
+        cls.addFunc( _SC("toInt")       , &DrawColor::toIntBindHelper);
+        cls.addFunc( _SC("fromInt")     , &DrawColor::fromIntBindHelper, staticMethod);
 
-        // cls.addFunc(_SC("rotate") , &DrawColor::rotate);
-        // cls.addFunc(_SC("rotateI"), &DrawColor::rotateI);
-
-
-        cls.addFunc(_SC("toInt")       , &DrawColor::toIntBindHelper);
-        cls.addFunc(_SC("fromInt")     , &DrawColor::fromIntBindHelper, staticMethod);
-
-        cls.addFunc(_SC("fromString")  , &DrawColor::fromString);
+        cls.addFunc( _SC("fromString")  , &DrawColor::fromString);
         cls.addFunc( _SC("toString")
                    , [](DrawColor* self) -> ssq::sqstring
                      {
                          MARTY_DC_BIND_SQUIRREL_ASSERT(self);
 
                          std::string strName = self->serialize();
+
                          #if !defined(SQUNICODE)
                              return strName;
                          #else
@@ -293,40 +286,125 @@ struct DrawColor : public ColorRef
                    );
 
 
-        // cls.addFunc(_SC("getMessage"), &DrawColor::getMessage);
-
-        // Это тоже - тут какая-то рекурсия возникает
-        // cls.addFunc(_SC("getMessageLowerCase"), [](CppClass* self) -> ssq::sqstring {
-        //     ssq::sqstring data = self->message;
-        //     //std::transform(data.begin(), data.end(), data.begin(), ::tolower);
-        //     return data;
-        // });
-
-        //cls.addVar(_SC("message"), &DrawColor::message);
         cls.addVar(_SC("r")     , &DrawColor::r);
         cls.addVar(_SC("g")     , &DrawColor::g);
         cls.addVar(_SC("b")     , &DrawColor::b);
         cls.addVar(_SC("a")     , &DrawColor::a);
 
-        cls.addVar(_SC("red")   , &DrawColor::r);
-        cls.addVar(_SC("green") , &DrawColor::g);
-        cls.addVar(_SC("blue")  , &DrawColor::b);
-        cls.addVar(_SC("alpha") , &DrawColor::a);
+
+        // алиасы с динными именами для r/g/b
+        // может, не делать с длинными именами?
+        // может, стоит сделать кучу статиков-алисов для именованных цветов?
+
+        // Пока не буду добавлять
+        // cls.addVar(_SC("red")   , &DrawColor::r);
+        // cls.addVar(_SC("green") , &DrawColor::g);
+        // cls.addVar(_SC("blue")  , &DrawColor::b);
+        // cls.addVar(_SC("alpha") , &DrawColor::a);
 
         return cls;
     }
 
-    // We will leave them public so Squirrel 
-    // can access them for the purpose of this example
-    // ssq::sqstring message;
-    // const int integer;
+
+}; // struct DrawColor
+
+//----------------------------------------------------------------------------
 
 
 
-};
+//----------------------------------------------------------------------------
+struct DrawCoords
+{
+    float x;
+    float y;
+
+    // DrawCoords() : x(0.0), y(0.0) {}
+    // DrawCoords(float x_, float y_) : x(x_), y(y_) {}
+    // DrawCoords(const DrawCoords &c) : x(c.x), y(c.y) {}
+    // DrawCoords(DrawCoords&&) = default;
+
+    static 
+    float convertHelper(ssq::Object &o, const SQChar *paramName)
+    {
+        (void)paramName;
+
+        if (o.isNull() || o.isEmpty())
+        {
+            return 0.0;
+        }
+
+        ssq::Type t = o.getType();
+        switch(t)
+        {
+            case ssq::Type::INTEGER:
+                return (float)o.toInt();
+            case ssq::Type::FLOAT:
+                return o.toFloat();
+            case ssq::Type::STRING:
+                {
+                    auto str = o.toString();
+                    return std::stof(str);
+                }
+
+            case ssq::Type::BOOL:
+            case ssq::Type::NULLPTR:
+            case ssq::Type::TABLE:
+            case ssq::Type::ARRAY:
+            case ssq::Type::USERDATA:
+            case ssq::Type::CLOSURE:
+            case ssq::Type::NATIVECLOSURE:
+            case ssq::Type::GENERATOR:
+            case ssq::Type::USERPOINTER:
+            case ssq::Type::THREAD:
+            case ssq::Type::FUNCPROTO:
+            case ssq::Type::CLASS:
+            case ssq::Type::INSTANCE:
+            case ssq::Type::WEAKREF:
+            case ssq::Type::OUTER:
+                [[fallthrough]];		
+            default: {}
+        }
+
+        throw ssq::TypeException("bad cast", ssq::typeToStr(ssq::Type::FLOAT), ssq::typeToStr(t));
+
+    }
+
+    static ssq::Class expose(ssq::VM& vm, const ssq::sqstring &className = _SC("DrawCoords"))
+    {
+        const bool staticMethod = true ;
+        const bool classMethod  = false;
+
+        auto cls = vm.addClass( className.c_str()
+                              , []( ssq::Object ox, ssq::Object oy  /* float x, float y */  ) -> DrawCoords*
+                                {
+                                    // (void)x;
+                                    // (void)y;
+                                    //  
+                                    // toInt(), toFloat()
+                                    //  
+                                    // isEmpty()
+                                    // isNull()
+
+                                    //return new DrawCoords{x, y};
+                                    //return new DrawCoords();
+
+                                    return new DrawCoords{convertHelper(ox, _SC("x")), convertHelper(oy, _SC("y"))};
+
+                                }
+                              , true // release
+                              );
+
+        cls.addVar(_SC("x"), &DrawCoords::x);
+        cls.addVar(_SC("y"), &DrawCoords::y);
+
+        return cls;
+    }
+
+}; // struct DrawCoords
 
 
 
+//----------------------------------------------------------------------------
 
 } // namespace simplesquirrel {
 
